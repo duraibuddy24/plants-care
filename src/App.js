dc658a6c4bf831e1dad39bc13b31263edb0209d8
Duraivel <durai.buddy24@gmail.com>
2025-08-30 22:43:43 +0530

    Intial commit with create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './Register';
+
+function App() {
+  return (
+    <div>
+      <Register/>
+      <p>gjhgjgjh</p>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/Register/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+const Register = ()=>{
+    const handleclick = ()=>{
+
+    }
+
+    return(
+        <div>
+            <p>Register</p>
+            <button>click</button>
+        </div>
+
+    )
+
+}
+
+export default Register
